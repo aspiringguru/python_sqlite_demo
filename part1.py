091b44b9f2fce58650de41c40c0240ffdf5b35fb
aspiringguru <bmatthewtaylor@gmail.com>
2018-06-17 01:29:03 +0000

    wip

--- part1.py
+++ part1.py
@@ -46,10 +46,10 @@ def select_all_tasks(c):
 
 def dynamic_data_entry():
     unix = time.time()
-    print ("unix:", type(unix), unix)
+    value = random.randrange(0,10)
+    print ("unix:", type(unix), unix, "value:", value)
     date = str(datetime.datetime.fromtimestamp(unix).strftime('%Y-%m-%d %H:%M:%S'))
     keyword = 'Python'
-    value = random.randrange(0,10)
     c.execute("INSERT into stufftoplot (unix, datestamp, keyword, value) VALUES (?, ?, ?, ?)", (unix, date, keyword, value))
     conn.commit()
 
@@ -80,22 +80,46 @@ def graph_data():
     #plt.show()
     plt.savefig("charts/output_chart.png")
     print("chart plotted to file")
+
+
+def del_and_update():
+    c.execute("SELECT * FROM stufftoplot")
+    temp = c.fetchall()
+    [print (row) for row in temp]
+    before = len(temp)
+    c.execute("SELECT * FROM stufftoplot WHERE value>5")
+    temp = c.fetchall()
+    num_matches = len(temp)
     
+    c.execute("UPDATE stufftoplot SET value=99 WHERE value=8")
+    conn.commit() 
+    c.execute("SELECT * FROM stufftoplot")
+    temp = c.fetchall()
+    [print (row) for row in temp] 
+    after = len(temp)
+    print ("before:", before)
+    print ("after:", after)
+    print ("num_matches:", num_matches)
+
+def create_n_rows(n):
+    for i in range(n):
+        dynamic_data_entry()
+        time.sleep(1) 
 
 create_table()
 #data_entry()
-data_insert(1111, "2016-01-02", "more keywords", 1)
-data_insert(2222, "2016-01-03", "less keywords", 2)
+#data_insert(1111, "2016-01-02", "more keywords", 1)
+#data_insert(2222, "2016-01-03", "less keywords", 2)
 
-dynamic_data_entry()
+#dynamic_data_entry()
 
-for i in range(100):
-    dynamic_data_entry()
-    time.sleep(1) 
+#    time.sleep(1) 
 
 #select_all_tasks(c)
-read_from_db()
-graph_data()
+#read_from_db()
+#graph_data()
+create_n_rows(10)
+del_and_update()
 
 c.close()
 conn.close()
